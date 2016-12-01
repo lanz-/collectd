428005a0975a621c866b49421fb102e0df6f84ab
Alexey Starkov <avs@qrator.net>
2016-12-01 23:27:17 +0300

    Removed duplicate function definition

--- src/write_graphite.c
+++ src/write_graphite.c
@@ -279,34 +279,6 @@ static int wg_send_buffer(struct wg_callback *cb) {
   return (0);
 }
 
-/* NOTE: You must hold cb->send_lock when calling this function! */
-static int wg_flush_nolock(cdtime_t timeout, struct wg_callback *cb) {
-  int status;
-
-  DEBUG("write_graphite plugin: wg_flush_nolock: timeout = %.3f; "
-        "send_buf_fill = %zu;",
-        (double)timeout, cb->send_buf_fill);
-
-  /* timeout == 0  => flush unconditionally */
-  if (timeout > 0) {
-    cdtime_t now;
-
-    now = cdtime();
-    if ((cb->send_buf_init_time + timeout) > now)
-      return (0);
-  }
-
-  if (cb->send_buf_fill == 0) {
-    cb->send_buf_init_time = cdtime();
-    return (0);
-  }
-
-  status = wg_send_buffer(cb);
-  wg_reset_buffer(cb);
-
-  return (status);
-}
-
 static int wg_callback_init(struct wg_callback *cb) {
   struct addrinfo *ai_list;
   cdtime_t now;
